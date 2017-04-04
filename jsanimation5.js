02ec52c8fdcf0b3b6c997cf442d8d0980cf5f7f7
Clint McCalla <clint.mccalla@gmail.com>
2017-04-04 15:14:17 -0500

    Create jsanimation5.js

---
+++ jsanimation5.js
@@ -0,0 +1,15 @@
+strokeWeight(3);
+stroke(57, 0, 214);
+fill(0, 210, 247);
+
+
+draw = function() {
+    
+    
+    background(255, 255, 255);
+    
+    ellipse(mouseX, mouseY, 30, 30);
+    
+};
+
+//program shows the ellipse in tracking with your mouse cursor
