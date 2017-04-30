66c562d716fc8e1b3fa08d76644ceace67972408
Clint McCalla <clint.mccalla@gmail.com>
2017-04-30 16:00:28 -0500

    Create reactelistenchg.js

---
+++ reactelistenchg.js
@@ -0,0 +1,20 @@
+//simple js to render cat picture which changes with onClick event to dog picture
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function makeDoggy (e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+var kitty = (
+	<img 
+		onClick={makeDoggy}
+    src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" />
+);
+
+ReactDOM.render(kitty, document.getElementById('app'));
