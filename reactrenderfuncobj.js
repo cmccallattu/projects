af59aeee32b8b783fd45c68704530f381190f695
Clint McCalla <clint.mccalla@gmail.com>
2017-05-01 15:07:59 -0500

    Create reactrenderfuncobj.js

---
+++ reactrenderfuncobj.js
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg"
+  }
+];
+
+var Friend = React.createClass( {
+  render: function() {
+    var friend = friends[0];
+    return(
+    <div>
+        <h1>
+          {friend.title}
+        </h1>
+        <img
+          src={friend.src} />
+      </div>
+    );
+  }
+})
+// New component class starts here:
+ReactDOM.render(<Friend/>, document.getElementById('app'));
