382cba5515076d99612bab451030b13e4445d53a
Clint McCalla <clint.mccalla@gmail.com>
2017-05-05 15:01:45 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -14,3 +14,18 @@ order by imdb_rating desc;  // ORDER BY method with DESC parameter
 select * from movies
 order by imdb_rating asc
 limit 3;  //  LIMIT method to reduce return of query to 3
+
+select price, count(*) from fake_apps //COUNT method which totals the number of rows in the data set then groups them by price
+group by price;
+
+select price, count(*) from fake_apps
+where downloads > 20000
+group by price;
+
+select sum(downloads) from fake_apps; //SUM method to total downloads within fake-apps data set
+
+select category, sum(downloads) from fake_apps //sums all downloads within given category and groups them by this category
+group by category;
+
+select name, category, min(downloads) from fake_apps //similar to MAX method except MIN method returns smallest downloads values
+group by category;
