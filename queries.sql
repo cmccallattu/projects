99f5ede8965859f80073a713e30b88e9326282c2
Clint McCalla <clint.mccalla@gmail.com>
2017-05-05 12:29:45 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-//where dealing with a movies database, demonstrating sql query 
+//where dealing with a movies database, demonstrating sql query methods
 
 select * from movies
 where year between 1990 and 2000
@@ -7,3 +7,6 @@ and genre = 'comedy';  // AND method
 select * from movies
 where genre = 'comedy'
 or year < 1980;  // OR method
+
+select * from movies
+order by imdb_rating desc; 
